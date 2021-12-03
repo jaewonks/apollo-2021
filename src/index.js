2aa9b98c29fca0e688691401ecd1a7951f459481
Jaewon Kim <jaewon@Jaewons-MacBook-Pro.local>
2021-12-03 15:50:48 +0000

    3th Dec

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from './apollo';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client} >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
